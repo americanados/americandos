eb49c04245a4b9b78da9d6244abea36a41139e20
antonio <antonio@antonio-PC>
2013-10-10 22:46:54 -0300

    Terceiro commit

--- module/Contato/src/Contato/Controller/HomeController.php
+++ module/Contato/src/Contato/Controller/HomeController.php
@@ -5,14 +5,15 @@ namespace Contato\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
-class HomeController extends AbstractActionController
-{
+class HomeController extends AbstractActionController {
 
-    public function indexAction()
-    {
+    public function indexAction() {
         return new ViewModel();
     }
 
+    public function sobreAction() {
+        return new ViewModel();
+    }
 
 }
 
